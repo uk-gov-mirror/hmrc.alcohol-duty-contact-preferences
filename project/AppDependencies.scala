4bbe217f4e7d687c8ecb69cd653907adad7f56bc
SachaMcCormack <175815565+SachaMcCormack@users.noreply.github.com>
2026-07-15 13:43:34 +0100

    changed email stub to true and fixed tests

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,16 +2,16 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.7.0"
+  private val bootstrapVersion = "10.8.0"
   private val hmrcMongoVersion = "2.12.0"
 
   val compile: Seq[ModuleID] = Seq(
     "org.typelevel"     %% "cats-core"                 % "2.13.0",
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"        % hmrcMongoVersion,
-    "com.beachape"      %% "enumeratum"                % "1.9.7",
-    "com.beachape"      %% "enumeratum-play"           % "1.9.7",
-    "com.beachape"      %% "enumeratum-cats"           % "1.9.7"
+    "com.beachape"      %% "enumeratum"                % "1.9.8",
+    "com.beachape"      %% "enumeratum-play"           % "1.9.8",
+    "com.beachape"      %% "enumeratum-cats"           % "1.9.8"
   )
 
   val test: Seq[ModuleID] = Seq(
